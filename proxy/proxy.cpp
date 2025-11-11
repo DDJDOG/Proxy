dd3d2ca688a682df935ab3c5cfd8fb6f9d6bb73d
GrowtopiaUnknown <88912186+DDJDOG@users.noreply.github.com>
2025-11-11 08:05:09 +0400

    Update proxy.cpp

--- proxy/proxy.cpp
+++ proxy/proxy.cpp
@@ -4,120 +4,129 @@
 #include <iostream>
 #include <sstream>
 #include <thread>
+#include <vector>
+#include <fstream>
+#include <regex>
+#include <windows.h>
 #include "enet/include/enet.h"
 #include "http.h"
 #include "server.h"
 #include "print.h"
-#include <conio.h>
-#include <fstream>
 #include "events.h"
 #include "utils.h"
-#include <regex>
 #include "proton/rtparam.hpp"
 #include "HTTPRequest.hpp"
+
 server* g_server = new server();
+
 using namespace std;
+
+// Handler to reset hosts file on Ctrl+C
 BOOL WINAPI HandlerRoutine(DWORD dwCtrlType)
 {
     print::set_text("\nFixing Hosts File!", LightGreen);
 
-    try
-    {
-        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+    try {
+        std::ofstream hostsFile("C:\\Windows\\System32\\drivers\\etc\\hosts", std::ios::trunc);
+        // Clears the hosts file safely
+        hostsFile.close();
+    }
+    catch (...) {}
 
-        if (dosyaYaz.is_open()) {
-            dosyaYaz << "";
-            dosyaYaz.close();
-        }
-    }
-    catch (std::exception)
-    {
-    }
-    return FALSE;
+    return FALSE; // pass control to next handler
 }
+
+// Utility to split string
 vector<string> split(const string& str, const string& delim)
 {
     vector<string> tokens;
     size_t prev = 0, pos = 0;
-    do
-    {
-        pos = str.find(delim, prev);
-        if (pos == string::npos) pos = str.length();
+
+    while ((pos = str.find(delim, prev)) != string::npos) {
         string token = str.substr(prev, pos - prev);
         if (!token.empty()) tokens.push_back(token);
         prev = pos + delim.length();
-    } while (pos < str.length() && prev < str.length());
+    }
+
+    string lastToken = str.substr(prev);
+    if (!lastToken.empty()) tokens.push_back(lastToken);
+
     return tokens;
 }
+
+// Setup Growtopia server and hosts file
 void setgtserver() {
-    try
-    {
-        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
+    try {
+        // Clear hosts file
+        std::ofstream hostsFile("C:\\Windows\\System32\\drivers\\etc\\hosts", std::ios::trunc);
+        hostsFile.close();
+    } catch (...) {}
 
-        if (dosyaYaz.is_open()) {
-            dosyaYaz << "";
-            dosyaYaz.close();
-        }
-    }
-    catch (std::exception)
-    {
-    }
-
-    try
-    {
+    try {
         http::Request request{ "http://a104-125-3-135.deploy.static.akamaitechnologies.com/growtopia/server_data.php" };
         const auto response = request.send("POST", "version=3.91&protocol=160&platform=0", { "Host: www.growtopia1.com" });
         rtvar var = rtvar::parse({ response.body.begin(), response.body.end() });
         var.serialize();
+
         if (var.find("server")) {
             g_server->m_port = var.get_int("port");
             g_server->portz = var.get_int("port");
-
         }
     }
-    catch (const std::exception& e)
-    {
+    catch (const std::exception& e) {
         std::cerr << "Request failed, error: " << e.what() << '\n';
     }
-    try
-    {
-        std::ofstream dosyaYaz("C:\\Windows\\System32\\drivers\\etc\\hosts");
 
-        if (dosyaYaz.is_open()) {
-            dosyaYaz << "127.0.0.1 growtopia1.com\n127.0.0.1 growtopia2.com\n127.0.0.1 www.growtopia1.com\n127.0.0.1 www.growtopia2.com";
-            dosyaYaz.close();
+    try {
+        // Write local proxy to hosts file
+        std::ofstream hostsFile("C:\\Windows\\System32\\drivers\\etc\\hosts", std::ios::trunc);
+        if (hostsFile.is_open()) {
+            hostsFile <<
+                "127.0.0.1 growtopia1.com\n"
+                "127.0.0.1 growtopia2.com\n"
+                "127.0.0.1 www.growtopia1.com\n"
+                "127.0.0.1 www.growtopia2.com";
+            hostsFile.close();
         }
-    }
-    catch (std::exception)
-    {
-    }
-
+    } catch (...) {}
 }
-int main() {
-    cout << "Last Update at 25.01.2024, today it gonna be updated fully. You can get this within joining SaveForwarder Discord Server: https://discord.gg/X56gwJdxkn" << endl;
-    system("start https://discord.gg/X56gwJdxkn");
-    SetConsoleTitleA("SrMotion Proxy ;)");
 
+int main() {
+    cout << "Last Update at 25.01.2024. Full update available via SaveForwarder Discord: https://discord.gg/X56gwJdxkn" << endl;
+    system("start https://discord.gg/X56gwJdxkn");
+
+    SetConsoleTitleA("SrMotion Proxy ;)");
     printf("Parsing the server_data.php\n");
-    SetConsoleCtrlHandler(HandlerRoutine, true);
+
+    SetConsoleCtrlHandler(HandlerRoutine, TRUE);
     setgtserver();
 
     system("Color a");
     printf("Based on enet by ama.\n");
+
     events::out::type2 = 2;
+
     g_server->ipserver = "127.0.0.1";
     g_server->create = "0.0.0.0";
-    std::thread http(http::run, g_server->ipserver, "17191");
-    http.detach();
+
+    std::thread httpThread(http::run, g_server->ipserver, "17191");
+    httpThread.detach();
+
     print::set_color(LightGreen);
-    enet_initialize();
+    if (enet_initialize() != 0) {
+        print::set_text("ENet initialization failed.\n", LightRed);
+        return 1;
+    }
+
     if (g_server->start()) {
-       print::set_text("Server & client proxy is running.\n", LightGreen);
-       while (true) {
-       g_server->poll();
-         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-       }
-  }
-   else
-    print::set_text("Failed to start server or proxy.\n", LightGreen);
+        print::set_text("Server & client proxy is running.\n", LightGreen);
+        while (true) {
+            g_server->poll();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    } else {
+        print::set_text("Failed to start server or proxy.\n", LightRed);
+    }
+
+    return 0;
 }
